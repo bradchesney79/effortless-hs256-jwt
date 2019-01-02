0175fbe107f3fb0c061f901583a6e01d050a20bf
BradChesney79 <bradchesney79@gmail.com>
2019-01-02 12:46:40 -0500

    add removing expired revocations logic

--- src/Ehjwt.php
+++ src/Ehjwt.php
@@ -301,38 +301,38 @@ class Ehjwt
         switch (json_last_error()) {
             case JSON_ERROR_NONE:
                 $error = ''; // JSON is valid // No error has occurred
-                break;
+                continue 2;
             case JSON_ERROR_DEPTH:
                 $error = 'The maximum stack depth has been exceeded.';
-                break;
+                continue 2;
             case JSON_ERROR_STATE_MISMATCH:
                 $error = 'Invalid or malformed JSON.';
-                break;
+                continue 2;
             case JSON_ERROR_CTRL_CHAR:
                 $error = 'Control character error, possibly incorrectly encoded.';
-                break;
+                continue 2;
             case JSON_ERROR_SYNTAX:
                 $error = 'Syntax error, malformed JSON.';
-                break;
+                continue 2;
             // PHP >= 5.3.3
             case JSON_ERROR_UTF8:
                 $error = 'Malformed UTF-8 characters, possibly incorrectly encoded.';
-                break;
+                continue 2;
             // PHP >= 5.5.0
             case JSON_ERROR_RECURSION:
                 $error = 'One or more recursive references in the value to be encoded.';
-                break;
+                continue 2;
             // PHP >= 5.5.0
             case JSON_ERROR_INF_OR_NAN:
                 $error = 'One or more NAN or INF values in the value to be encoded.';
-                break;
+                continue 2;
             case JSON_ERROR_UNSUPPORTED_TYPE:
                 $error = 'A value of a type that cannot be encoded was given.';
-                break;
+                continue 2;
             default:
                 $error = 'Unknown JSON error occured.';
-                break;
-    }
+                continue 2;
+        }
 
         if ($error !== '') {
             var_dump('undecodable header');
@@ -347,34 +347,34 @@ class Ehjwt
             case JSON_ERROR_NONE:
                 // JSON is valid, no error has occurred
                 $error = '';
-                break;
+                continue 2;
             case JSON_ERROR_DEPTH:
                 $error = 'The maximum stack depth has been exceeded.';
-                break;
+                continue 2;
             case JSON_ERROR_STATE_MISMATCH:
                 $error = 'Invalid or malformed JSON.';
-                break;
+                continue 2;
             case JSON_ERROR_CTRL_CHAR:
                 $error = 'Control character error, possibly incorrectly encoded.';
-                break;
+                continue 2;
             case JSON_ERROR_SYNTAX:
                 $error = 'Syntax error, malformed JSON.';
-                break;
+                continue 2;
             case JSON_ERROR_UTF8:
                 $error = 'Malformed UTF-8 characters, possibly incorrectly encoded.';
-                break;
+                continue 2;
             case JSON_ERROR_RECURSION:
                 $error = 'One or more recursive references in the value to be encoded.';
-                break;
+                continue 2;
             case JSON_ERROR_INF_OR_NAN:
                 $error = 'One or more NAN or INF values in the value to be encoded.';
-                break;
+                continue 2;
             case JSON_ERROR_UNSUPPORTED_TYPE:
                 $error = 'A value of a type that cannot be encoded was given.';
-                break;
+                continue 2;
             default:
                 $error = 'Unknown JSON error occured.';
-                break;
+                continue 2;
     }
 
         if ($error !== '') {
@@ -418,7 +418,23 @@ class Ehjwt
             // 'Revoked'
 
             // clean out revoked token records if the UTC unix time ends in "0"
-            if ((0 + 0) === (substr($utcTimeNow, -1) + 0)) {
+            if (0 == (substr($utcTimeNow, -1) + 0)) {
+
+                try {
+                    $dbh = new PDO($this->config['dsn'], $this->config['dbUser'], $this->config['dbPassword'], array(PDO::ATTR_PERSISTENT => true ));
+                    
+                    $stmt = $dbh->prepare("DELETE FROM revoked_ehjwt WHERE exp =< $utcTimeNow");
+                    
+                    $stmt->execute();
+
+                    $dbh = null;
+                    $stmt = null;
+                }
+
+                catch (PDOException $e) {
+                    //print "Error!: " . $e->getMessage() . "<br/>";
+                    die();
+                }
 
             }
 
