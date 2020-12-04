da3f5999f20a1753d0413d44366016c78d9dbd6d
BradChesney79 <bradchesney79@gmail.com>
2020-12-03 22:06:03 -0500

    tweak testing to align with code changes

--- tests/unit/ehjwtTest.php
+++ tests/unit/ehjwtTest.php
@@ -59,81 +59,141 @@ class ehjwtTest extends TestCase
         $this->assertTrue(true);
     }
 
-    /**
-    * @covers ::__construct
-    */
+//    /**
+//    * @covers ::__construct
+//    */
+//
+//    public function testObjectInstanceIsCreated()
+//    {
+//        $jwt = new EHJWT('jwtSecret', '', 'DSNString','DBUser', 'DBPassword', 'BradChesney.com', 'user');
+//        $this->assertInstanceOf(EHJWT::class, $jwt);
+//    }
+//
+//    public function testDsnArgumentSettings ()
+//    {
+//        $jwt = new EHJWT('jwtSecret', '', 'DSNString','DBUser', 'DBPassword', 'BradChesney.com', 'user');
+//
+//        $jwt->createToken();
+//        $expectedToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ1c2VyIiwiaXNzIjoiQnJhZENoZXNuZXkuY29tIn0.R3NfZXdBdjhaQW1xMkpSc3E1d3p4NnE2M2F6WW55WFd4UGlrdDJtMUpPcw';
+//        $this->assertEquals($expectedToken, $jwt->getToken());
+//    }
+//
+//    public function testClaimsAreAdded() {
+//        $jwt = new EHJWT('jwtSecret', '', 'DSNString','DBUser', 'DBPassword', 'BradChesney.com', 'user');
+//        $jwt->addOrUpdateIatProperty('10000');
+//        $jwt->addOrUpdateNbfProperty('0');
+//        $jwt->addOrUpdateSubProperty('1000');
+//        $jwt->addOrUpdateJtiProperty('1');
+//        $jwt->addOrUpdateExpProperty('1887525317');
+//
+//        $jwt->addOrUpdateCustomClaim('age', '39');
+//        $jwt->addOrUpdateCustomClaim('sex', 'male');
+//        $jwt->addOrUpdateCustomClaim('location', 'Davenport, Iowa');
+//
+//        $jwt->createToken();
+//        $claims = $jwt->getTokenClaims();
+//
+//        $this->assertEquals('BradChesney.com', $claims['iss']);
+//        $this->assertEquals('user', $claims['aud']);
+//        $this->assertEquals('10000', $claims['iat']);
+//        $this->assertEquals('0', $claims['nbf']);
+//        $this->assertEquals('1000', $claims['sub']);
+//        $this->assertEquals('1', $claims['jti']);
+//        $this->assertEquals('1887525317', $claims['exp']);
+//
+//        $this->assertEquals('39', $claims['age']);
+//        $this->assertEquals('male', $claims['sex']);
+//        $this->assertEquals('Davenport, Iowa', $claims['location']);
+//    }
+//
+//    public function testValidateToken()
+//    {
+//        $jwt = new EHJWT('jwtSecret', '', 'mysql:host=localhost;dbname=EHJWT', 'brad', 'password', 'BradChesney.com', 'user');
+//        $jwt->addOrUpdateIatProperty('10000');
+//        $jwt->addOrUpdateNbfProperty('0');
+//        $jwt->addOrUpdateSubProperty('1000');
+//        $jwt->addOrUpdateJtiProperty('1');
+//        $jwt->addOrUpdateExpProperty('1887525317');
+//
+//        $jwt->addOrUpdateCustomClaim('age', '39');
+//        $jwt->addOrUpdateCustomClaim('sex', 'male');
+//        $jwt->addOrUpdateCustomClaim('location', 'Davenport, Iowa');
+//
+//        $jwt->createToken();
+//
+//        $tokenString = $jwt->getToken();
+//
+//        unset($jwt);
+//
+//        $jwt2 = new EHJWT('jwtSecret', '', 'mysql:host=localhost;dbname=EHJWT', 'brad', 'password', 'BradChesney.com', 'user');
+//        $jwt2->loadToken($tokenString);
+//        $itVerks = $jwt2->validateToken();
+//        $this->assertEquals(true, $itVerks);
+//    }
+//
+//    public function testLoadingConfigFile() {
+//
+//        $this->assertFileDoesNotExist('custom-config-conf.php');
+//
+//        if(!file_exists('custom-config-conf.php')) {
+//            // create custom config file
+//            $customConfigFile = fopen("custom-config-conf.php", "w");
+//
+//            // fill file with config info
+//
+//            fwrite($customConfigFile, "<?php\n");
+//            fwrite($customConfigFile, "return [\n");
+//            fwrite($customConfigFile, "'dsn' => 'mysql:host=localhost;dbname=ehjwt',\n");
+//            fwrite($customConfigFile, "'dbUser' => 'brad',\n");
+//            fwrite($customConfigFile, "'dbPassword' => 'password',\n");
+//            fwrite($customConfigFile, "'jwtSecret' => 'Secret',\n");
+//            fwrite($customConfigFile, "'iss' => 'BradChesney.com',\n");
+//            fwrite($customConfigFile, "'aud' => 'users',\n");
+//            fwrite($customConfigFile, "'disallowArguments' => 'false'\n");
+//            fwrite($customConfigFile, "];");
+//
+//            //fwrite($customConfigFile, $txt);
+//            fclose($customConfigFile);
+//
+//            // create token using config file
+//
+//            $jwt = new EHJWT('', 'custom-config-conf.php', '', '', '', '', '');
+//            $jwt->addOrUpdateIatProperty('10000');
+//            $jwt->addOrUpdateNbfProperty('0');
+//            $jwt->addOrUpdateSubProperty('1000');
+//            $jwt->addOrUpdateJtiProperty('1');
+//            $jwt->addOrUpdateExpProperty('1887525317');
+//
+//            $jwt->addOrUpdateCustomClaim('age', '39');
+//            $jwt->addOrUpdateCustomClaim('sex', 'male');
+//            $jwt->addOrUpdateCustomClaim('location', 'Davenport, Iowa');
+//
+//
+//            // delete custom config file
+//            if (!unlink('custom-config-conf.php')) {
+//                $this->assertFileDoesNotExist('custom-config-conf.php');
+//            }
+//
+//            $jwt->createToken();
+//            $claims = $jwt->getTokenClaims();
+//
+//            $this->assertEquals('BradChesney.com', $claims['iss']);
+//            $this->assertEquals('users', $claims['aud']);
+//            $this->assertEquals('10000', $claims['iat']);
+//            $this->assertEquals('0', $claims['nbf']);
+//            $this->assertEquals('1000', $claims['sub']);
+//            $this->assertEquals('1', $claims['jti']);
+//            $this->assertEquals('1887525317', $claims['exp']);
+//
+//            $this->assertEquals('39', $claims['age']);
+//            $this->assertEquals('male', $claims['sex']);
+//            $this->assertEquals('Davenport, Iowa', $claims['location']);
+//        }
+//    }
 
-    public function testObjectInstanceIsCreated()
-    {
-        $jwt = new EHJWT('jwtSecret', '', 'DSNString','DBUser', 'DBPassword', 'BradChesney.com', 'user');
-        $this->assertInstanceOf(EHJWT::class, $jwt);
-    }
+    public function testLoadingConfigFileDisallowArguments() {
 
-    public function testDsnArgumentSettings ()
-    {
-        $jwt = new EHJWT('jwtSecret', '', 'DSNString','DBUser', 'DBPassword', 'BradChesney.com', 'user');
-
-        $jwt->createToken();
-        $expectedToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ1c2VyIiwiaXNzIjoiQnJhZENoZXNuZXkuY29tIn0.R3NfZXdBdjhaQW1xMkpSc3E1d3p4NnE2M2F6WW55WFd4UGlrdDJtMUpPcw';
-        $this->assertEquals($expectedToken, $jwt->getToken());
-    }
-
-    public function testClaimsAreAdded() {
-        $jwt = new EHJWT('jwtSecret', '', 'DSNString','DBUser', 'DBPassword', 'BradChesney.com', 'user');
-        $jwt->addOrUpdateIatProperty('10000');
-        $jwt->addOrUpdateNbfProperty('0');
-        $jwt->addOrUpdateSubProperty('1000');
-        $jwt->addOrUpdateJtiProperty('1');
-        $jwt->addOrUpdateExpProperty('1887525317');
-
-        $jwt->addOrUpdateCustomClaim('age', '39');
-        $jwt->addOrUpdateCustomClaim('sex', 'male');
-        $jwt->addOrUpdateCustomClaim('location', 'Davenport, Iowa');
-
-        $jwt->createToken();
-        $claims = $jwt->getTokenClaims();
-
-        $this->assertEquals('BradChesney.com', $claims['iss']);
-        $this->assertEquals('user', $claims['aud']);
-        $this->assertEquals('10000', $claims['iat']);
-        $this->assertEquals('0', $claims['nbf']);
-        $this->assertEquals('1000', $claims['sub']);
-        $this->assertEquals('1', $claims['jti']);
-        $this->assertEquals('1887525317', $claims['exp']);
-
-        $this->assertEquals('39', $claims['age']);
-        $this->assertEquals('male', $claims['sex']);
-        $this->assertEquals('Davenport, Iowa', $claims['location']);
-    }
-
-    public function testValidateToken()
-    {
-        $jwt = new EHJWT('jwtSecret', '', 'mysql:host=localhost;dbname=EHJWT', 'brad', 'password', 'BradChesney.com', 'user');
-        $jwt->addOrUpdateIatProperty('10000');
-        $jwt->addOrUpdateNbfProperty('0');
-        $jwt->addOrUpdateSubProperty('1000');
-        $jwt->addOrUpdateJtiProperty('1');
-        $jwt->addOrUpdateExpProperty('1887525317');
-
-        $jwt->addOrUpdateCustomClaim('age', '39');
-        $jwt->addOrUpdateCustomClaim('sex', 'male');
-        $jwt->addOrUpdateCustomClaim('location', 'Davenport, Iowa');
-
-        $jwt->createToken();
-
-        $tokenString = $jwt->getToken();
-
-        unset($jwt);
-
-        $jwt2 = new EHJWT('jwtSecret', '', 'mysql:host=localhost;dbname=EHJWT', 'brad', 'password', 'BradChesney.com', 'user');
-        $jwt2->loadToken($tokenString);
-        $itVerks = $jwt2->validateToken();
-        $this->assertEquals(true, $itVerks);
-    }
-
-    public function testLoadingConfigFile() {
-
-        $this->assertFileNotExists('custom-config-conf.php');
+        $this->assertFileDoesNotExist('custom-config-conf.php');
 
         if(!file_exists('custom-config-conf.php')) {
             // create custom config file
@@ -155,9 +215,29 @@ class ehjwtTest extends TestCase
             //fwrite($customConfigFile, $txt);
             fclose($customConfigFile);
 
+            // create standard config file
+
+            $customConfigFile = fopen("ehjwt-conf.php", "w");
+
+            // fill file with config info
+
+            fwrite($customConfigFile, "<?php\n");
+            fwrite($customConfigFile, "return [\n");
+            fwrite($customConfigFile, "'dsn' => 'mysql:host=localhost;dbname=ehjwt',\n");
+            fwrite($customConfigFile, "'dbUser' => 'stdDonald',\n");
+            fwrite($customConfigFile, "'dbPassword' => 'stcPassword',\n");
+            fwrite($customConfigFile, "'jwtSecret' => 'stdSecret',\n");
+            fwrite($customConfigFile, "'iss' => 'grifterinchief.com',\n");
+            fwrite($customConfigFile, "'aud' => 'users',\n");
+            fwrite($customConfigFile, "'disallowArguments' => 'true'\n");
+            fwrite($customConfigFile, "];");
+
+            //fwrite($customConfigFile, $txt);
+            fclose($customConfigFile);
+
             // create token using config file
 
-            $jwt = new EHJWT('', 'custom-config-conf.php', '', '', '', '', '');
+            $jwt = new EHJWT('not-secret', 'custom-config-conf.php', 'no-good-dsn', 'not-brad', 'not-password', 'not-site', 'not-user');
             $jwt->addOrUpdateIatProperty('10000');
             $jwt->addOrUpdateNbfProperty('0');
             $jwt->addOrUpdateSubProperty('1000');
@@ -171,7 +251,7 @@ class ehjwtTest extends TestCase
 
             // delete custom config file
             if (!unlink('custom-config-conf.php')) {
-                assertFileNotExists('custom-config-conf.php');
+                $this->assertFileDoesNotExist('custom-config-conf.php');
             }
 
             $jwt->createToken();
@@ -220,6 +300,43 @@ class ehjwtTest extends TestCase
         $this->assertEquals('39', $claims['age']);
         $this->assertEquals('male', $claims['sex']);
         $this->assertEquals('Davenport, Iowa', $claims['location']);
+    }
+
+    public function testClearingCustomClaims() {
+
+        // create token using env vars
+
+        $jwt = new EHJWT();
+        $jwt->addOrUpdateIatProperty('10000');
+        $jwt->addOrUpdateNbfProperty('0');
+        $jwt->addOrUpdateSubProperty('1000');
+        $jwt->addOrUpdateJtiProperty('1');
+        $jwt->addOrUpdateExpProperty('1887525317');
+
+        $jwt->addOrUpdateCustomClaim('age', '39');
+        $jwt->addOrUpdateCustomClaim('sex', 'male');
+        $jwt->addOrUpdateCustomClaim('location', 'Davenport, Iowa');
+
+        $jwt->clearCustomClaim('location');
+
+        $jwt->createToken();
+        $claims = $jwt->getTokenClaims();
+
+        $this->assertArrayNotHasKey('location', $claims);
+
+    }
+
+    public function testMissingSignature() {
+        $this->expectException('RuntimeException');
+        $this->expectExceptionMessage('Token does not contain three delimited sections');
+
+        $token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ1c2VyIiwiaXNzIjoiQnJhZENoZXNuZXkuY29tIn0';
+        $jwt = new EHJWT();
+        $jwt->loadToken($token);
+//        $this->expectException(RuntimeException::class);
+//        $this->expectExceptionMessage('Token does not contain three delimited sections');
+//        $jwt->validateToken();
+
     }
 
 //    public function testCreateToken()
@@ -646,17 +763,17 @@ class ehjwtTest extends TestCase
 //        //$dsn = addslashes(mysql:host=localhost);
 //        $dsn = 'mysql:host=localhost';
 //
-//        putenv('ESJWT_DSN=' . $dsn);
+//        putenv('EHJWT_DSN=' . $dsn);
 //
-//        putenv('ESJWT_DB_USER=root');
+//        putenv('EHJWT_DB_USER=root');
 //
-//        putenv('ESJWT_DB_PASS=password');
+//        putenv('EHJWT_DB_PASS=password');
 //
-//        putenv('ESJWT_JWT_SECRET=secrets');
+//        putenv('EHJWT_JWT_SECRET=secrets');
 //
-//        putenv('ESJWT_ISS=rustbeltrebellion.com');
+//        putenv('EHJWT_ISS=rustbeltrebellion.com');
 //
-//        putenv('ESJWT_AUD=list255.com');
+//        putenv('EHJWT_AUD=list255.com');
 //
 //        $now = time();
 //        $expires = time() + 30 * 60;
@@ -691,19 +808,19 @@ class ehjwtTest extends TestCase
 //        //$dsn = addslashes(mysql:host=localhost);
 //        $dsn = 'mysql:host=localhost';
 //
-//        putenv('ESJWT_DSN=' . $dsn);
+//        putenv('EHJWT_DSN=' . $dsn);
 //
-//        putenv('ESJWT_DB_USER=root');
+//        putenv('EHJWT_DB_USER=root');
 //
-//        putenv('ESJWT_DB_PASS=password');
+//        putenv('EHJWT_DB_PASS=password');
 //
-//        putenv('ESJWT_JWT_SECRET=secrets');
+//        putenv('EHJWT_JWT_SECRET=secrets');
 //
-//        putenv('ESJWT_ISS=rustbeltrebellion.com');
+//        putenv('EHJWT_ISS=rustbeltrebellion.com');
 //
-//        putenv('ESJWT_AUD=list255.com');
+//        putenv('EHJWT_AUD=list255.com');
 //
-//        putenv('ESJWT_USE_ENV_VARS=true');
+//        putenv('EHJWT_USE_ENV_VARS=true');
 //
 //        $now = time();
 //        $expires = time() + 30 * 60;
@@ -731,19 +848,19 @@ class ehjwtTest extends TestCase
 //        $this->assertEquals($claims['aud'], $checkValues['aud']);
 //
 //
-//        putenv('ESJWT_DSN');
+//        putenv('EHJWT_DSN');
 //
-//        putenv('ESJWT_DB_USER');
+//        putenv('EHJWT_DB_USER');
 //
-//        putenv('ESJWT_DB_PASS');
+//        putenv('EHJWT_DB_PASS');
 //
-//        putenv('ESJWT_JWT_SECRET');
+//        putenv('EHJWT_JWT_SECRET');
 //
-//        putenv('ESJWT_ISS');
+//        putenv('EHJWT_ISS');
 //
-//        putenv('ESJWT_AUD');
+//        putenv('EHJWT_AUD');
 //
-//        putenv('ESJWT_USE_ENV_VARS');
+//        putenv('EHJWT_USE_ENV_VARS');
 //    }
 }
 
